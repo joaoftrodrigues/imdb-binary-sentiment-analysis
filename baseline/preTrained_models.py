cb7cdf45389d8b55cb2745d6402215a7df2f1424
joaoftrodrigues <joaoftrodrigues@gmail.com>
2023-03-19 18:14:09 +0000

    Added Stanza, but highly cost to compute

--- baseline/preTrained_models.py
+++ baseline/preTrained_models.py
@@ -1,5 +1,6 @@
 from textblob import TextBlob
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import stanza 
 
 
 def tb_sentiment(text):
@@ -11,6 +12,13 @@ def vader_sentiment_withObject(obj, text):
     """ Returns raw value from Vader Sentiment polarity.
      Implies there's a vaderSentiment object """
     return obj.polarity_scores(text)['compound']
+
+
+def stanza_sentiment(obj, text):
+    """ Returns raw value from Stanza polarity """
+
+    # Used index 0, because each input outputs a single value
+    return obj(text).sentences[0].sentiment
 
 
 def get_dataset_polarities(texts, model):
@@ -38,6 +46,16 @@ def predict_dataset_labels(texts, model):
     elif model == 'vaderSentiment':
         vader_analyzer = SentimentIntensityAnalyzer()
         return [polarity_to_label(vader_sentiment_withObject(vader_analyzer, text)) for text in texts]
+    
+    elif model == 'stanza':
+        stanza_analyzer = stanza.Pipeline(lang='en', processors='tokenize,sentiment', tokenize_no_ssplit=False)
+
+        # Values from stanze ranges from 0 to 2;
+        # by subtracting 1, it gets normalized with other models (-1 to 1)
+        return [polarity_to_label(stanza_sentiment(stanza_analyzer, text)-1) for text in texts]
+
+    else:
+        return 0
 
     
 
---
+++ baseline/stanza_eval.py
@@ -0,0 +1,15 @@
+import stanza 
+import pandas as pd
+import preTrained_models
+
+# Read test data
+test_ds = pd.read_csv('../data/imdb_reviews_test.csv')
+
+# Label using TextBlob
+predicted_labels = preTrained_models.predict_dataset_labels(test_ds['text'], 'stanza')
+
+# Accuracy obtained from TextBlob
+tb_acc = preTrained_models.accuracy_score(predicted_labels, test_ds['label'])
+
+# Show accuracy
+print(tb_acc)
