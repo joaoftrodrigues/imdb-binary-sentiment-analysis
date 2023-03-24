18d7d8db5e3b0b7d5abf5bbda94b69abc28533ce
joaoftrodrigues <joaoftrodrigues@gmail.com>
2023-03-24 00:53:55 +0000

    Adjusted to use updated function

--- lexicon_sentiments.py
+++ lexicon_sentiments.py
@@ -1,12 +1,13 @@
 import nltk 
 import pandas as pd
 import lexicon_tools
+from lib import spacy_preprocessing
 
 # Read test data
 test_ds = pd.read_csv('data/imdb_reviews_test.csv')
 
 # Label reviews based on lexicon analysis
-predicted_labels = lexicon_tools.lexicon_analysis(test_ds['text'])
+predicted_labels = spacy_preprocessing.preprocess_and_evaluation_nocorr(test_ds['text'])
 
 # Accuracy obtained
 accuracy = lexicon_tools.models_tools.accuracy_score(predicted_labels, test_ds['label'])
