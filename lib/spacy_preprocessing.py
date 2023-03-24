14f892aafd38851da924cb7a68f6b3e469bada71
joaoftrodrigues <joaoftrodrigues@gmail.com>
2023-03-24 00:52:07 +0000

    Added function that preprocess and label, each iteration

--- lib/spacy_preprocessing.py
+++ lib/spacy_preprocessing.py
@@ -1,5 +1,26 @@
 import spacy 
 import contextualSpellCheck
+from lib import models_tools
+import pandas as pd 
+
+
+def ncr_initializer(filepath):
+    """ Reads NCR file and transforms it in dict
+        of word / score """
+
+    # Import NCR-lexicon
+    ncr_lexicon = pd.read_csv(filepath)
+
+    # Set Words as index and remove id column
+    ncr_lexicon.set_index('English', inplace=True)
+
+    # Resume sentiment values to single column, with values -1, 0, 1
+    lex = ncr_lexicon['Positive'] - ncr_lexicon['Negative']
+
+    # Transform to Dictionary, for easier manipulation
+    lex = lex.to_dict()
+
+    return lex
 
 
 def preprocessing(texts):
@@ -27,7 +48,7 @@ def preprocessing(texts):
         processed_text = []
 
         # Present entities in current text        
-        entities_found = [str(entity) for entity in text_features.ents]
+        #entities_found = [str(entity) for entity in text_features.ents]
 
         for token in text_features:
 
@@ -36,7 +57,7 @@ def preprocessing(texts):
                 continue
 
             # Entities
-            if token.text in entities_found:
+            if token.ent_type:
                 continue
 
             # Add lowercase token to list
@@ -46,3 +67,50 @@ def preprocessing(texts):
         processed_texts.append(processed_text)
 
     return processed_texts
+
+    
+def preprocess_and_evaluation_nocorr(texts, filepath='lexicons/NCR-lexicon.csv'):
+    """ Preprocessing + label using spacy """
+
+    # Load model
+    nlp = spacy.load('en_core_web_sm')
+
+    # Store texts after processing
+    predicted_labels = []
+
+    # Initialize NCR lexicon and transform to use
+    lex = ncr_initializer(filepath)
+
+    total = len(texts)
+    n_text = 0
+
+    for text in texts:
+        
+        # Retrieval of information from spacy
+        text_features = nlp(text)
+
+        text_polarity = 0
+
+        for token in text_features:
+
+            # Numerals and Punctuation
+            if token.pos in ['NUM', 'PUNCT']:
+                continue
+
+            # Entities
+            if token.ent_type:
+                continue       
+
+            # Polarity of word, from dictionary
+            word_polarity = lex.get(token.lemma_)
+
+            # Add when is not None
+            if word_polarity:
+                text_polarity += word_polarity    
+        
+        # Add tokens to work, from text
+        predicted_labels.append(models_tools.polarity_to_label(text_polarity))
+        n_text += 1
+        print(f"{n_text}/{total}")
+
+    return predicted_labels
